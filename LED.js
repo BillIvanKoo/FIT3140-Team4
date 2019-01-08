085429a3b0191ad2b362b0e5fdddd3f55282e3e3
Sriharan Hariharan <shar88@student.monash.edu>
2019-01-08 19:53:56 +0800

    added turn off functionality to LED Class

--- LED.js
+++ LED.js
@@ -1,17 +1,29 @@
 var b = require("bonescript")
+
 class LED {
     constructor(ledName) {
         this.name = ledName
         this.state = 0
+        this.interval = null
         b.pinMode(ledName, 'out')
     }
     
     turnOn() {
         this.state = 1
         var this1 = this;
-        setInterval(function() {
+        this.interval = setInterval(function() {
             b.digitalWrite(this1.name, this1.state)
         }, 15)
     }
+    
+    turnOff() {
+        var this1 = this
+        if (this.interval != null) {
+            this.state = 0
+            clearInterval(this1.interval)
+            this.interval = null
+        }
+    }
 }
-module.exports = LED
+
+module.exports = LED;

c4599edd8a003f585d2b7b1b9e3d13223e2e73a4
Bill Ivan Kooslarto <kooslarto.bill.ivan@gmail.com>
2019-01-23 15:58:01 +0800

    configured vue js and element io with webpack for ui

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-header>
+    <img v-bind:src="logo">
+    </el-header>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Header from 'element-ui';
+Vue.use(Header)
+
+import logo from './assets/logo.png'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'Hello, Vue!',
+      logo
+    };
+  }
+};
+</script>

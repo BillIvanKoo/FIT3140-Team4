ac30356f1594024a0951a020a98a66db9d57c7ea
Bill Ivan Kooslarto <kooslarto.bill.ivan@gmail.com>
2019-01-30 18:30:09 +0800

    Added seconds unit in input field

--- client/src/components/ResponseToolbar.vue
+++ client/src/components/ResponseToolbar.vue
@@ -1,7 +1,8 @@
 <template>
     <el-footer>
         <el-checkbox v-model="timed" class="footer-item">Timed Alarm</el-checkbox>
-        <el-input-number v-model="sec" :min="1" :disabled="!timed" controls-position="right" class="footer-item"/>
+        <el-input-number v-model="sec" :min="1" :disabled="!timed" controls-position="right" class="footer-item" size="small"/>
+        <span v-if="timed">s</span>
         <el-button type="warning" @click="soundAlarm" class="footer-item">Sound Alarm</el-button>
         <el-button type="danger" @click="$emit('warn', 'lock_safe')" class="footer-item">Lock Safe</el-button>
     </el-footer>
@@ -35,6 +36,15 @@ export default {
 
 <style scoped>
     .footer-item {
-        margin: 5px
+        margin: 5px;
+    }
+    span {
+        position: relative;
+        left: -55px;
+        z-index: 12;
+        margin: -5px;
+        padding: 0;
+        color: #606266;
+        font-style: arial;
     }
 </style>
